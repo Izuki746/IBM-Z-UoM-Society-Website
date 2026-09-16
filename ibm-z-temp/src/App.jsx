9add18a6744965de8675eae6d4b8b12b811556d3
Viral Chandiramani <viral.chandiramani@student.manchester.ac.uk>
2026-09-16 23:46:20 +0000

    Navbar finishing touches

--- ibm-z-temp/src/App.jsx
+++ ibm-z-temp/src/App.jsx
@@ -14,6 +14,35 @@ function App() {
 
   return (
     <>
+    <main>
+
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#events">Events</a></li>
+          <li><a href="#knowledge">Knowledge</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#society">Society</a></li>
+        </ul>
+      </nav>
+
+
+      <section className="hero">
+        <div className='initial-text' data-depth="1"><p>Enterprise Computing redefined</p></div>
+        <div className="hero-content" data-depth="2">
+          <h1>IBM Z Society</h1>
+          <h2>University of Manchester</h2>
+          <p>Explore the world of mainframe computing and join our community of enthusiasts.</p>
+        </div>
+        <div className="text-overlay" data-depth="3">
+          <h4>Explore.Build.Connect</h4>
+        </div>
+        <div className="status" data-depth="1">
+          <p>Status</p>
+        </div>
+        <a href="#about" className="cta-button" data-depth="2">Learn More</a>
+      </section>
+    </main>
       <Cursor />
     </>
   )
--- ibm-z-temp/src/main.jsx
+++ ibm-z-temp/src/main.jsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@fontsource/ibm-plex-sans/400.css'
+import '@fontsource/ibm-plex-sans/600.css'
+import '@fontsource/ibm-plex-sans/700.css'
 
+import '@fontsource/ibm-plex-mono/400.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
